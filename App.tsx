839ae463f4858b981bd523aafb5444f17ece4829
spagdoon0411 <spandan.suthar@live.com>
2024-04-28 22:45:17 -0700

    assorted font changes; no sucess with font loading

--- App.tsx
+++ App.tsx
@@ -1,35 +1,28 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-import Card from './assets/Components/Card'
-import { useFonts } from 'expo-font'
+import { StyleSheet, View, Text, ScrollView } from 'react-native'
+import ExerciseCard from './Components/Exercises'
+// import { useFonts } from 'expo-font'
 
 export default function App() {
-	const [fontsLoaded] = useFonts({
-		'IBMPlexSansCondensed-Bold': require('./assets/fonts/IBMPlexSansCondensed-Bold.ttf'),
-		'IBMPlexSansCondensed-Regular': require('./assets/fonts/IBMPlexSansCondensed-Regular.ttf'),
-		'IBMPlexSansCondensed-Medium': require('./assets/fonts/IBMPlexSansCondensed-Medium.ttf'),
-		'IBMPlexSansCondensed-SemiBold': require('./assets/fonts/IBMPlexSansCondensed-SemiBold.ttf'),
-		'IBMPlexSansCondensed-Light': require('./assets/fonts/IBMPlexSansCondensed-Light.ttf'),
-		'IBMPlexSansCondensed-ExtraLight': require('./assets/fonts/IBMPlexSansCondensed-ExtraLight.ttf'),
-		'IBMPlexSansCondensed-Italic': require('./assets/fonts/IBMPlexSansCondensed-Italic.ttf'),
-		'IBMPlexSansCondensed-Thin': require('./assets/fonts/IBMPlexSansCondensed-Thin.ttf'),
-		'IBMPlexSansCondensed-ThinItalic': require('./assets/fonts/IBMPlexSansCondensed-ThinItalic.ttf'),
-		'IBMPlexSansCondensed-LightItalic': require('./assets/fonts/IBMPlexSansCondensed-LightItalic.ttf'),
-		'IBMPlexSansCondensed-ExtraLightItalic': require('./assets/fonts/IBMPlexSansCondensed-ExtraLightItalic.ttf'),
-		'IBMPlexSansCondensed-SemiBoldItalic': require('./assets/fonts/IBMPlexSansCondensed-SemiBoldItalic.ttf'),
-		'IBMPlexSansCondensed-MediumItalic': require('./assets/fonts/IBMPlexSansCondensed-MediumItalic.ttf'),
-		'IBMPlexSansCondensed-BoldItalic': require('./assets/fonts/IBMPlexSansCondensed-BoldItalic.ttf'),
-	})
-
-	if (!fontsLoaded) {
-		; <Text>Fonts could not be loaded</Text>
-	}
-
 	return (
 		<View style={styles.container}>
-			<Card heading='TestHeading'>
-				<Text>Card Component</Text>
-			</Card>
+			<Text>{'Scheduled Workouts'}</Text>
+			<ScrollView contentContainerStyle={styles.scroll}>
+				<ExerciseCard
+					name='Arms and Chest'
+					tags={['Biceps', 'Triceps', 'Pecs']}
+					exerciseNames={['Pulldowns', 'Pullups', 'Dips']}
+				>
+					<Text>TestChild</Text>
+				</ExerciseCard>
+				<ExerciseCard
+					name='Legs'
+					tags={['Quads', 'Hamstrings', 'Glutes']}
+					exerciseNames={['Squats', 'Lunges', 'Deadlifts']}
+				>
+					<Text>TestChild</Text>
+				</ExerciseCard>
+			</ScrollView>
 		</View>
 	)
 }
@@ -38,7 +31,12 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: '#fff',
-		alignItems: 'center',
+		width: '100%',
+	},
+	scroll: {
+		flex: 1,
+		width: '100%',
 		justifyContent: 'center',
+		alignItems: 'center',
 	},
 })
