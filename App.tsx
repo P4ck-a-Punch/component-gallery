b1dbc11fe1fe762ed97d22a302d33da24074705c
spagdoon0411 <spandan.suthar@live.com>
2024-05-10 05:52:27 -0700

    introduce tags to demo

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import Card from 'Componants/Card'
 import Page from 'Componants/Page'
+import Tags from 'Componants/Tags'
 import CardContainer from 'Componants/CardContainer'
 import React from 'react'
 import { Text } from 'react-native'
@@ -9,6 +10,7 @@ const App = () => {
 		<Page title={'Beans'}>
 			<CardContainer>
 				<Card heading={'Beans'}>
+					<Tags tags={['Beans', 'Legumes', 'Protein']} />
 					<Text></Text>
 				</Card>
 			</CardContainer>
