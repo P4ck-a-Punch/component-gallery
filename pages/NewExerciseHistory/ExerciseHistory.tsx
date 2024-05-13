9e2cae500e14c7fc022d9172ab6476b39df4473d
spagdoon0411 <spandan.suthar20042011@gmail.com>
2024-05-13 11:45:52 -0700

    refactor functions and add documentation

--- pages/NewExerciseHistory/ExerciseHistory.tsx
+++ pages/NewExerciseHistory/ExerciseHistory.tsx
@@ -4,12 +4,25 @@ import React from 'react'
 import { View } from 'react-native'
 import MetricReport from './MetricReport'
 
-const ExerciseHistory = () => {
+type ExerciseHistoryProps = {
+	exerciseId: number
+}
+
+/*
+ * A page displaying the history of a specific exercise, across all 
+ * metrics. TODO: map over all metrics for the exercise to create 
+ * multiple MetricReport components.
+ *
+ * @param {ExerciseHistoryProps} props the properties for the page
+ * @param {number} props.exerciseId the ID of the exercise
+ * @return {React.JSX.Element} the page component
+ */
+const ExerciseHistory = (props: ExerciseHistoryProps) => {
 	return (
 		<Page title='Bicep Curls'>
 			<View style={{ paddingTop: 30 }}></View>
 			<CardContainer>
-				<MetricReport exerciseId={1} metricId={1} />
+				<MetricReport exerciseId={props.exerciseId} metricId={1} />
 			</CardContainer>
 		</Page>
 	)
